e0202f53af98fe2ee0ea8fbb07e479dee5285c12
WahdelMW <WWoodhouse@spartaglobal.com>
2021-05-14 13:46:28 +0100

    implemented method doesTermsOfServiceShow

--- src/test/java/com/sparta/room1/pom/pages/ShippingPageImpl.java
+++ src/test/java/com/sparta/room1/pom/pages/ShippingPageImpl.java
@@ -3,20 +3,22 @@ package com.sparta.room1.pom.pages;
 import com.sparta.room1.pom.pages.interfaces.AddressPage;
 import com.sparta.room1.pom.pages.interfaces.PaymentPage;
 import com.sparta.room1.pom.pages.interfaces.ShippingPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ShippingPageImpl extends Page implements ShippingPage {
 
     private WebDriver driver;
+    private By termsOfService = By.linkText("(Read the Terms of Service)");
 
     public ShippingPageImpl(WebDriver driver) {
         this.driver = driver;
     }
 
-
     @Override
     public boolean doesTermsOfServiceShow() {
-        return false;
+        String termsOfServiceText = driver.findElement(termsOfService).getText();
+        return termsOfServiceText.contains("http://automationpractice.com/index.php?id_cms=3&controller=cms&content_only=1");
     }
 
     @Override
